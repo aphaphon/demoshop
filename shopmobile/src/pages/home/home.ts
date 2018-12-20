1b5111c69c6450eef5c824a1c2d5723ef1246737
aphaphon <pui.aphaphon@gmail.com>
2018-12-20 21:18:41 +0700

    Complete.

--- shopmobile/src/pages/home/home.ts
+++ shopmobile/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private products: Product[];
+  private ProductOrderId: string;
+  private ProductOrderAmount: number;
 
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get<Product[]>("http://localhost:5000/api/Product").subscribe(
+      it => {
+        console.log(it)
+        this.products = it;
+      });
+  }
+
+  OrderProduct(){
+    this.http.post("http://localhost:5000/api/Order",
+    {
+        Id: this.ProductOrderId,
+        Amount: this.ProductOrderAmount
+    }).subscribe(
+        it => {
+        // SUCCESS: Do something
+        console.log('success')
+        }, 
+        error => {
+            // ERROR: Do something
+            console.log("fail")
+        });
   }
 
 }
